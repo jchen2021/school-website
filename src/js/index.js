d127435f359ad4f7684ac85cb754a673e8e962b0
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-06-22 01:58:25 -0400

    Revert "nav done for laptop and desktop"
    This reverts commit 98e01abc78ee136c49310ae359ad914069342d91.

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,1 @@
 import AOS from "aos";
-
-function hamburgerMenu() {
-    var hamburger = document.querySelector(".hamburger");
-    var navMenu = document.querySelector(".nav-tabs");
-
-    hamburger.addEventListener("click", function () {
-        hamburger.classList.toggle("is-active");
-        navMenu.classList.toggle("nav-active");
-    });
-
-}
-
