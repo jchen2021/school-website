2c412b40ebbaa0991ef81780911c688a616f7218
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-06-23 11:22:56 -0400

    cb3 and 4
    needs a lil more fixing

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
+import "aos/dist/aos.css";
 import AOS from "aos";
-
-function hamburgerMenu() {
-    var hamburger = document.querySelector(".hamburger");
-    var navMenu = document.querySelector(".nav-tabs");
-
-    hamburger.addEventListener("click", function () {
-        hamburger.classList.toggle("is-active");
-        navMenu.classList.toggle("nav-active");
-    });
-
-}
-
+AOS.init();
